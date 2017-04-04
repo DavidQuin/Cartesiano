441a255114d7e3ed4eb36eb2e952707c4d666a3e
Dvd <Dvd@Dvd-PC>
2017-04-04 01:15:52 -0600

    Elimine las excepciones porque no eran necesarias. Todo se porcentajeará al tamaño del canvas, sin necesidad de pasarle cuánto este canvas mide.

--- src/Graficador/Pintor.java
+++ src/Graficador/Pintor.java
@@ -12,30 +12,38 @@ public class Pintor {
     
     private Canvas lienzo;
     
-    Graphics2D pantallaDeDibujo;
+    private Graphics2D pantallaDeDibujo;
     
-    int tamaño;
+    private int tamaño;
+    
+    //---- Posiciones fundamentales del lienzo.
+    private float margenEnX;
+    private float margenEnY;
+    private float largoEjeX;
+    private float largoEjeY;
+    
+    
     
     
     public void pasarElCanvas(Canvas lienzo){
         this.lienzo = lienzo;
     }
+
     
-    public void pasarTamañodePintado(int tamaño) throws TamañoMayorQueHeightOWidthExcetion, CanvasEsNull{
-        this.tamaño = tamaño;
-        
-            if (tamaño > lienzo.getHeight() || tamaño > lienzo.getWidth()){
-                throw new TamañoMayorQueHeightOWidthExcetion("El Tamaño No puede ser Mayor que el Largo o el Ancho del Canvas.");
-            }
-            
-            if (lienzo == null){
-                throw new CanvasEsNull("Aún no ha asignado ningún Lienzo al objeto Pintor.");
-            }
-        
-    }
     
     private void obtenerPantalladeDibujo(){
         pantallaDeDibujo = (Graphics2D) lienzo.getGraphics();
+    }
+    
+    
+    private void ConfigurarLienzo(){
+        
+        margenEnX = ConversorPuntoALienzo.obtenerMargenX(tamaño);
+        margenEnY = ConversorPuntoALienzo.obtenerMargenY(tamaño);
+        
+        
+        
+        
     }
     
     
